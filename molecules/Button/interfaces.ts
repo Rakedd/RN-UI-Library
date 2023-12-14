c880797ced7a40d7b490f549a28fa3cb9343c49f
Rayan Keddache <Rayan.keddache@epitech.eu>
2023-12-14 01:09:29 +0100

    [Button]: New empty component button

---
+++ molecules/Button/interfaces.ts
@@ -0,0 +1,7 @@
+export interface Props extends ButtonStylesType {
+
+}
+
+export interface ButtonStylesType {
+
+}
---
+++ molecules/Button/useStyles.tsx
@@ -0,0 +1,14 @@
+import React, {useMemo} from 'react'
+import {StyleSheet} from 'react-native'
+
+import {useTheme} from '@theme'
+
+export const useStyles = () => {
+  const theme = useTheme()
+  const styles = useMemo(() => StyleSheet.create({
+    container: {
+      
+    }
+  }), [theme])
+  return styles
+}
